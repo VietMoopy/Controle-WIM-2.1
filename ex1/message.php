d7a61fb9aef03814b2db8e1172c1e1b7547d9f42
alexandrenguyen68817 <alexandrenguyen68817@gmail.com>
2016-06-15 03:42:20 -0400

    Added conditions to message.php

--- ex1/message.php
+++ ex1/message.php
@@ -1,30 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<link href="http://maxcdn.bootstrapcdn.com/bootstrap/2.3.0/css/bootstrap.min.css" rel="stylesheet" />
-		<meta charset="UTF-8" />
-		<title></title>
-	</head>
-	<body>
-		<div class="container-fluid">
-			<form action="score.php" method="post">
-				<fieldset>
-					<legend>Langue</legend>
-					<h5>Choose a language :</h5>
-					<label class="radio">
-						<input type="radio" name="french"  value="fr" >French
-					</label>
-					<label class="radio">
-						<input type="radio" name="english"  value="en">English
-					</label>
-					<label class="radio">
-						<input type="radio" name="spanish"  value="sp">Spanish
-					</label>
-
-					<button class="btn">Envoyer</button>
-
-				</fieldset>
-				<input type="hidden" value="3" name="nbq">
-			</form>
-		</body>
-	</html>
+<?php
+	 if(isset($_POST['bouton'])){
+     	 extract ( $_POST );
+       if($choix == "fr"){
+         echo "Comment vas-tu ?";
+       }
+      else if($choix == "en"){
+        echo "How do you do ?";
+      }
+     else{
+       echo htmlentities("¿ Cómo estás ?", ENT_COMPAT, 'UTF-8');
+      }
+		}
+?>
